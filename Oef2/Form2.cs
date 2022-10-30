89c081a90ba1a7fc83bb219f76645407fa3b89cd
Tetez10 <101556702+Tetez10@users.noreply.github.com>
2022-10-30 02:28:52 +0200

    trying to fix

--- Oef2/Form2.cs
+++ Oef2/Form2.cs
@@ -12,12 +12,35 @@ namespace Oef2
 {
     public partial class Form2 : Form
     {
+
+        List<int> WaardeLijst = new List<int>();
         public Form2()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserDialog window = new UserDialog();
+            DialogResult result = window.ShowDialog();
 
-    }
+            // als de vennster ok is dan gaat je de waarde die in het venster staat toevoegen aan mijn lijst van waarde
+            if(window.DialogResult == DialogResult.OK)
+            {
+                AddToYourList(window.waarde);
+            }
 
-}
+
+            }
+
+        public void AddToYourList(int waarde)
+        {
+            WaardeLijst.items.Add(waarde);
+        }
+
+
+
+
+        } }
+
+
--- Oef2/UserDialog.Designer.cs
+++ Oef2/UserDialog.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Oef2
 {
-    partial class Form2
+    partial class UserDialog
     {
         /// <summary>
         /// Required designer variable.
@@ -30,56 +30,33 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(630, 175);
+            this.label1.Location = new System.Drawing.Point(366, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 15);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Gemiddelde";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Waarde";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(614, 193);
+            this.textBox1.Location = new System.Drawing.Point(345, 123);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 7;
+            this.textBox1.TabIndex = 1;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(328, 306);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "nieuwe getal";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(87, 122);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 94);
-            this.listBox1.TabIndex = 5;
-            // 
-            // Form2
+            // UserDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
-            this.Name = "Form2";
-            this.Text = "Form2";
+            this.Controls.Add(this.label1);
+            this.Name = "UserDialog";
+            this.Text = "UserDialog";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,7 +66,5 @@
 
         private Label label1;
         private TextBox textBox1;
-        private Button button1;
-        private ListBox listBox1;
     }
 }
